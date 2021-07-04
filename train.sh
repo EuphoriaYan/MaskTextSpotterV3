4b256ff57158e05e516ed59212de02172aae22a1
Euphoria Yan <ysq58000@foxmail.com>
2021-07-04 10:55:47 +0800

    update

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0,3
 # python -m torch.distributed.launch --nproc_per_node=8 tools/train_net.py --config-file configs/pretrain/seg_rec_poly_fuse_feature.yaml
-python -m torch.distributed.launch --nproc_per_node=1 tools/train_net.py --config-file configs/mixtrain/seg_rec_poly_fuse_feature_for_fakepages.yaml
+python -m torch.distributed.launch --nproc_per_node=2 tools/train_net.py --config-file configs/mixtrain/seg_rec_poly_fuse_feature_for_fakepages.yaml
---
+++ train_no_distributed.sh
@@ -0,0 +1,2 @@
+export CUDA_VISIBLE_DEVICES=3
+python tools/train_net.py --config-file configs/mixtrain/seg_rec_poly_fuse_feature_for_fakepages.yaml
